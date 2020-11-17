04ebf849bf399229e7a5952e5377b6b1b6e43b8b
Brian Mann <brian.mann86@gmail.com>
2020-11-17 16:43:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('time-require')
+
+console.time('init')
+
+require('bluebird')
+require('lodash')
+require('ramda')
+require('gulp')
+require('gulp-complexity')
+require('inquirer')
+require('rxjs')
+require('underscore')
+require('eslint')
+require('yeoman-generator')
+
+console.timeEnd('init')

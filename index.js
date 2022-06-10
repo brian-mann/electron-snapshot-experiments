100df79018793bf5292721f6e4e005b5bdc9cf91
Brian Mann <brian.mann86@gmail.com>
2022-06-10 11:49:57 -0400

    add programmatic cpu profiling

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const fs = require('fs')
+const v8Profiler = require('v8-profiler-next')
+
+const title = 'electron-snapshot-experiments'
+
 require('time-require')
 
 console.time('init')
+
+// console.profile('foo')
+
+v8Profiler.setGenerateType(1)
+
+v8Profiler.startProfiling(title, true)
 
 require('bluebird')
 require('lodash')
@@ -13,4 +24,13 @@ require('underscore')
 require('eslint')
 require('yeoman-generator')
 
+const profile = v8Profiler.stopProfiling(title)
+
+// console.profileEnd('foo')
+
 console.timeEnd('init')
+
+profile.export((err, result) => {
+  fs.writeFileSync(`${title}.cpuprofile`, result);
+  profile.delete();
+})

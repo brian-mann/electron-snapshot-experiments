d4dbc7b1c355d057525fb7c1d6d560a4d31ad3c4
Brian Mann <brian.mann86@gmail.com>
2022-06-10 12:38:32 -0400

    use v8-profiler-next + inspector-profile

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 const fs = require('fs')
 const v8Profiler = require('v8-profiler-next')
+const Inspector = require('inspector-api')
+const inspector = new Inspector()
 
 const title = 'electron-snapshot-experiments'
 
-require('time-require')
+;(async () => {
+  require('time-require')
 
-console.time('init')
+  console.time('init')
 
-// console.profile('foo')
+  // console.profile('foo')
+  await inspector.profiler.enable()
+  await inspector.profiler.start()
 
-v8Profiler.setGenerateType(1)
+  v8Profiler.setGenerateType(1)
 
-v8Profiler.startProfiling(title, true)
+  v8Profiler.startProfiling(title, true)
 
-require('bluebird')
-require('lodash')
-require('ramda')
-require('gulp')
-require('gulp-complexity')
-require('inquirer')
-require('rxjs')
-require('underscore')
-require('eslint')
-require('yeoman-generator')
+  require('bluebird')
+  require('lodash')
+  require('ramda')
+  require('gulp')
+  require('gulp-complexity')
+  require('inquirer')
+  require('rxjs')
+  require('underscore')
+  require('eslint')
+  require('yeoman-generator')
 
-const profile = v8Profiler.stopProfiling(title)
+  console.timeEnd('init')
 
-// console.profileEnd('foo')
+  const v8Prof = v8Profiler.stopProfiling(title)
 
-console.timeEnd('init')
+  // console.profileEnd('foo')
 
-profile.export((err, result) => {
-  fs.writeFileSync(`${title}.cpuprofile`, result);
-  profile.delete();
-})
+  const inspectorProf = await inspector.profiler.stop()
+
+  fs.writeFileSync(`${title}-inspector-profile.cpuprofile`, JSON.stringify(inspectorProf))
+
+  v8Prof.export((err, result) => {
+    fs.writeFileSync(`${title}-v8-profile.cpuprofile`, result)
+
+    v8Prof.delete()
+  })
+})()
+
